3ae62f6938d91775817927d30b0a622bfe34adb8
J W <woodjon@gmail.com>
2018-07-16 16:20:14 -0400

    Update base URL for API

--- model-client/src/App.js
+++ model-client/src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import { Container } from 'reactstrap';
 import Prediction from "./components/Prediction";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Container>
+      <Fragment>
         <header className="header">
           <h1>Salary Prediction</h1>
         </header>
-        <Prediction/>
-      </Container>
+        <Container>
+          <Prediction/>
+        </Container>
+      </Fragment>
     );
   }
 }
--- model-client/src/components/Prediction.js
+++ model-client/src/components/Prediction.js
@@ -1,22 +1,59 @@
-import React, {Component} from  'react';
-import { Row, Col } from 'reactstrap';
+import React, {Component, Fragment} from  'react';
+import { Row, Col, Button } from 'reactstrap';
+import axios from "../utils/axios-model";
 
 class Prediction extends Component {
+  state = {
+    predictedSalary: '',
+    years: ''
+  };
+
+  predict = () => {
+    axios.get(`/${this.state.years}`)
+      .then(response => this.setState({predictedSalary: response.data}))
+      .catch(response => {
+        console.log(response);
+      });
+  };
+
+  yearsChanged = e => this.setState({years: e.target.value});
 
   render() {
+    let formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    });
+
+    let prediction = "";
+
+    if(this.state.predictedSalary) {
+      prediction = (<Row>
+        <Col xs="6">
+          <p>You should earn {formatter.format(this.state.predictedSalary)}</p>
+        </Col>
+      </Row>);
+    }
+
     return (
-      <Row>
-        <Col xs="12">
-          <Col xs="3">
+      <Fragment>
+        <Row>
+          <Col xs="6">
             <p className="App-intro">
               Enter the number of years:
             </p>
           </Col>
-          <Col xs="3">
-            <input type="text" />
+          <Col xs="6">
+            <input type="text" onChange={this.yearsChanged}/>
           </Col>
-        </Col>
-      </Row>
+        </Row>
+        <Row>
+          <Col xs="4">
+            <Button onClick={e => this.predict(this.state.predictedSalary)} color="primary">Predict</Button>
+          </Col>
+        </Row>
+        {prediction}
+      </Fragment>
     );
   }
 }
